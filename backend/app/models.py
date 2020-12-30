0716e0bd392693ab370c1af5d42e36e241290c3d
markakisn <markakisn@users.noreply.github.com>
2020-12-30 13:54:09 +0200

    configured migration

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,7 +11,20 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    is_owner = db.Column(db.Boolean, default=False, nullable=False)
+
     password_hash = db.Column(db.String(256), nullable=False)
     token = db.Column(db.String(32), nullable=True, default=None, unique=True)
+
+    
+    first_name = db.Column(db.String(64), nullable=False )
+    last_name = db.Column(db.String(64), nullable=False )
+    country = db.Column(db.String(64), nullable=False )
+    city = db.Column(db.String(64), nullable=False)
+    street = db.Column(db.String(64), nullable=False )
+    number = db.Column(db.Integer, nullable=False )
+    zip_code =  db.Column(db.String(5), nullable=False )
+
