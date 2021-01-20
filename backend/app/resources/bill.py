bac21d3ef2602b6889b99f99a505f47c3516e6c8
markakisn <markakisn@users.noreply.github.com>
2021-01-20 11:05:22 +0200

    malakies ekana

--- backend/app/resources/bill.py
+++ backend/app/resources/bill.py
@@ -26,6 +26,35 @@ bill_schema =BillSchema()
 def update_bills(user_id):
     sessions = Session.query.filter(Session.user_id==user_id).order_by(Session.starting_time)
     bills = Bill.query.filter(Bill.user_id ==user_id).order_by(Bill.period_start_date.desc())
+
+    if sessions.count() == 0:
+        return
+
+    
+
+    
+    
+    last_bill = bills.first()
+    last_bill_date = '0000-00-00 00:00:00'
+
+    if last_bill is not None:
+        last_bill_date = last_bill.period_end_date
+
+    for ses in sessions.all():
+        #print(ses.starting_time[0:7]+'-00') 
+        '''
+        if ses.finishing_time > last_bill_date:
+            b = Bill(
+                user_id= int(user_id)
+                period_start_date = ses.starting_time[0:7]+'-00'
+                period_end_date = ses.starting_time[0:7]+'-31'   #tha einai panta 31 opote mhn tupwseis ending date sto frontend apla tupose oti e;inai gia ;ena mhna
+                total_cost_ = #provlimaaa
+                is_paid = Flase
+            )
+        '''
+
+
+
     pass
 
 class BillResource(Resource):
