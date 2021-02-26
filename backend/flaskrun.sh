4803b389bbfde6d5f55329372f25f5df09d79410
markakisn <markakisn@users.noreply.github.com>
2021-02-26 22:45:40 +0200

    added stationByUser resource, nefeli gia sena <3

--- backend/flaskrun.sh
+++ backend/flaskrun.sh
@@ -1,3 +1,4 @@
 # Run the server (NOT in the background, so we can kill it with a simple CTR-C) with the https certificates created above
 #just for lazy testing the https certificates
 flask run --cert=localhost.crt --key=localhost.key 
+$SHELL
