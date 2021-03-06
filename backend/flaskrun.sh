18e9b756697232cb94c348dfbbe53b8e6cdfb196
markakisn <markakisn@users.noreply.github.com>
2021-03-06 19:54:52 +0200

    added $SHELL command to flaskrun gia na mhn kleinei sinexia

--- backend/flaskrun.sh
+++ backend/flaskrun.sh
@@ -1,3 +1,4 @@
 # Run the server (NOT in the background, so we can kill it with a simple CTR-C) with the https certificates created above
 #just for lazy testing the https certificates
 flask run --cert=cert.pem --key=key.pem 
+$SHELL
